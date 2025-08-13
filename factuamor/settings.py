bc78a10d2c8db25180198c638f83d5778a236c14
lykandev <likantropo89@hotmail.com>
2025-08-12 20:58:06 -0500

    fix: Corregir configuración de plantillas en settings.py
    Se ha corregido la configuración de TEMPLATES en settings.py para utilizar la convención estándar de Django.
    - Se ha eliminado la línea DIRS para permitir que Django busque automáticamente las plantillas dentro de las aplicaciones con APP_DIRS: True.

--- factuamor/settings.py
+++ factuamor/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'factuamor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'factuamor/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
