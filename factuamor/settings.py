2418b5abbf3fb4dc7be0095de4f85172a517cca2
lykandev <likantropo89@hotmail.com>
2025-08-12 21:04:24 -0500

    fix: Corregir configuración de plantillas en settings.py
    Se ha corregido la configuración de TEMPLATES en settings.py.
    - Se ha eliminado la línea DIRS para permitir que Django busque automáticamente las plantillas dentro de las aplicaciones.
    - Se ha añadido el procesador de contexto django.template.context_processors.debug.

--- factuamor/settings.py
+++ factuamor/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -62,6 +61,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug'
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
