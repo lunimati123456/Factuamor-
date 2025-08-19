63e347fe775719422f4f9d11d09428b6574a7620
lykandev <likantropo89@hotmail.com>
2025-08-18 20:38:36 -0500

    feat: Implementar la funcionalidad para crear nuevos productos
    Se ha agregado la funcionalidad para que el administrador pueda crear nuevos productos a través de una vista basada en clase de Django. Los cambios incluyen:
    - Creación de la vista ProductoCreateView para manejar el formulario.
    - Adición de la URL crear_producto en productos/urls.py.
    - Creación de la plantilla producto_form.html para la interfaz del formulario.

--- productos/urls.py
+++ productos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductoListView
+from .views import ProductoListView, ProductoCreateView
 
 app_name = 'productos'
 
 urlpatterns = [
     path('lista/', ProductoListView.as_view(), name='lista_productos'),
+    path('crear/', ProductoCreateView.as_view(), name='crear_producto'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from .models import Producto
 
-# La vista de inicio ahora buscará la plantilla en la nueva ubicación
+# La vista de inicio
 def index(request):
     return render(request, 'index.html')
 
+# Vista para la lista de productos
 class ProductoListView(ListView):
     model = Producto
     template_name = 'productos/producto_list.html'
     context_object_name = 'productos'
 
+# Vista para crear un nuevo producto
+class ProductoCreateView(CreateView):
+    model = Producto
+    fields = ['nombre', 'descripcion', 'precio', 'stock', 'imagen']
+    template_name = 'productos/producto_form.html'
+    success_url = reverse_lazy('productos:lista_productos')
