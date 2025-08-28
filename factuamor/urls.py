56e7859d9557b5657a86bf4bdf01bb708f2de669
lykandev <likantropo89@hotmail.com>
2025-08-28 10:19:06 -0500

    feat: Implementar sistema de login y logout; mejorar navbar dinámico

--- factuamor/urls.py
+++ factuamor/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('factura/', include('factura.urls', namespace='factura')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     ]
 
 # Configuración para servir archivos de medios y estáticos en desarrollo
